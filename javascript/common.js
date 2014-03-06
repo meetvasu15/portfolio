7489d670cbd5d4553a15e651af09afd403e76481
Vasu Gupta <meetvasu15@gmail.com>
2014-03-06 02:09:24 -0700

    load of changes

--- javascript/common.js
+++ javascript/common.js
@@ -1,5 +1,26 @@
- 
- $('#scene').parallax();
+ $( document ).ready(function(){
+ $('#scene').parallax(); 
+ $("html").niceScroll({scrollspeed : "100", autohidemode : false});
+ $(".headerLinksContainer span").click(function(e){
+		var divId = e.target.id; 
+		if($("#"+divId+"Container")){
+		$.scrollTo($("#"+divId+"Container"),1000);
+		}
+  });
+  var headLoc = $('.headerLinksContainer').offset().top;
+  $(window).bind('scroll', function () { 
+		if ($(window).scrollTop() > headLoc) { 
+			$('header').addClass('fixed');
+		} else {
+			$('header').removeClass('fixed');
+		}
+	});
+ });
 // var scene = document.getElementById('scene');
 //var parallax = new Parallax(scene);
- 
+
+function onDownArrowClick(){ 
+$.scrollTo($(".statementContainer"),1000);
+//$("body").localScroll($(".parallaxContainer"));
+}
+
